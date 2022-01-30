49d8f89f3ec98ce2fed299c1bd4d08630da684d5
Aaron <aapl365@utexas.edu>
2022-01-29 20:10:58 -0800

    Adding basic support for checking low-traffic times.

--- Src/Main.py
+++ Src/Main.py
@@ -6,24 +6,26 @@ if __name__ == '__main__':
     AZURE_SHARE_CONFIG_FILE_PATH = './Config/azureShareConfig.json'
     BACKUP_QUEUE_FILE_PATH = './Config/backupQueue.txt'
 
-    if not Scheduler.is_low_traffic_time():
-        print('Not low traffic time. Ending')
-        exit(1)
-
     client = AzureShareClient.init_by_file(AZURE_SHARE_CONFIG_FILE_PATH)
 
     backup_queue = BackupQueue(BACKUP_QUEUE_FILE_PATH)
 
     try:
         while True:
+            if not Scheduler.is_low_traffic_time():
+                print('Not low traffic time. Ending')
+                exit(0)
+
             try:
                 (src_path, dest_path) = backup_queue.dequeue()
-                if dest_path is not None:
-                    client.upload_file(str(src_path), dest_path=str(dest_path))
-                else:
-                    client.upload_file(str(src_path))
-
             except IndexError:
+                print('DONE. No items in queue?')
                 break
+
+            if dest_path is not None:
+                client.upload_file(str(src_path), dest_path=str(dest_path))
+            else:
+                client.upload_file(str(src_path))
+
     except Exception as e:
         print('FAIL exited main loop: ', e)
--- Src/Scheduler.py
+++ Src/Scheduler.py
@@ -1,12 +1,19 @@
+from datetime import datetime, time
+
+import pytz
 
 class Scheduler:
     """
     Determines what times network traffic is low/high.
     """
 
-    _LOW_TRAFFIC_START = ''
-    _LOW_TRAFFIC_END = ''
+    _LOW_TRAFFIC_START_ISO = '02:00:00'
+    _LOW_TRAFFIC_END_ISO = '05:00:00'
 
     @staticmethod
-    def is_low_traffic_time() -> bool:
-        return True
+    def is_low_traffic_time(timezone_str: str='US/Pacific') -> bool:
+        tz = pytz.timezone(timezone_str)
+        time_now = datetime.now(tz).time()
+        low_traffic_start = time.fromisoformat(Scheduler._LOW_TRAFFIC_START_ISO)
+        low_traffic_end = time.fromisoformat(Scheduler._LOW_TRAFFIC_END_ISO)
+        return low_traffic_start < time_now < low_traffic_end
